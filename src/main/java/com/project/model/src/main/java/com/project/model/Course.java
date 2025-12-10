5c6f1221fbc2a6ecbf1e232d669cc4392ee33e7c
cansuuilhan <154470556+cansuuilhan@users.noreply.github.com>
2025-12-11 00:08:56 +0300

    Course sınıfı oluşturuldu

---
+++ src/main/java/com/project/model/src/main/java/com/project/model/Course.java
@@ -0,0 +1,36 @@
+package com.project.model;
+
+public class Course {
+    private String courseId;
+    private String courseName;
+    private int credit;
+    private String instructorName;
+
+    public Course(String courseId, String courseName, int credit, String instructorName) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.credit = credit;
+        this.instructorName = instructorName;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    @Override
+    public String toString() {
+        return courseId + " - " + courseName + " (" + credit + " credits, Instructor: " + instructorName + ")";
+    }
+}
