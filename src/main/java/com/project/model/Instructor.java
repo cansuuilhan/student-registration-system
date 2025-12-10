706f448515ee520448f52e62ed5697e905e0ffee
cansuuilhan <154470556+cansuuilhan@users.noreply.github.com>
2025-12-11 00:14:04 +0300

    Instructor sınıfı oluşturuldu

---
+++ src/main/java/com/project/model/Instructor.java
@@ -0,0 +1,30 @@
+package com.project.model;
+
+public class Instructor {
+    private String id;
+    private String name;
+    private String department;
+
+    public Instructor(String id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name + " (" + department + ")";
+    }
+}
