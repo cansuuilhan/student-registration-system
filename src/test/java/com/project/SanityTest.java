390aadb19456bae85fbd2eeaffcf3cb040610f8b
cansu ilhan <cansuuiilhan@gmail.com>
2026-01-05 04:13:43 +0300

    Maven yapılandırması ve JUnit test altyapısı eklendi

---
+++ src/test/java/com/project/SanityTest.java
@@ -0,0 +1,12 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SanityTest {
+
+    @Test
+    void testFrameworkWorks() {
+        assertTrue(true);
+    }
+}
